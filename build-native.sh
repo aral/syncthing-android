41b3646144771ed729c7f4de66f78bfc5d06868f
Felix Ableitner <me@nutomic.com>
2014-06-13 19:59:56 +0200

    wip (ref #12)

--- build-native.sh
+++ build-native.sh
@@ -1,27 +1,42 @@
-#!/bin/bash
+#!/bin/sh
 
-cd "$GOPATH/src/github.com/calmh/syncthing/"
+cd "./syncthing"
 
-./build.sh test || exit 1
-./build.sh assets
+echo "Setting GOPATH, GOROOT and GOBIN..."
+export GOPATH=$(pwd)/gopath
+mkdir -p $GOPATH/root
+export GOROOT=$GOPATH/root
+mkdir -p $GOPATH/bin
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
+echo "Installing godep..."
+go get github.com/tools/godep
+echo "Installing go vet..."
+go get code.google.com/p/go.tools/cmd/vet
+echo "Creating a symbolic link..."
+mkdir -p $GOPATH/src/github.com/calmh
+rm -f $GOPATH/src/github.com/calmh/syncthing
+ln -s $(pwd) $GOPATH/src/github.com/calmh/syncthing
+
+rm -f syncthing syncthing-x86 syncthing-armeabi-v7a syncthing-armeabi syncthing-mips
 
 export GOOS=linux
 
 export GOARCH=386
 
 ./build.sh
-cp syncthing syncthing-x86
+mv syncthing syncthing-x86
 
 export GOARCH=arm
 
 export GOARM=7
-./build.sh
-cp syncthing syncthing-armeabi-v7a
+build
+mv syncthing syncthing-armeabi-v7a
 
 export GOARM=5
-./build.sh
-cp syncthing syncthing-armeabi
+build
+mv syncthing syncthing-armeabi
 
-#export GOARCH=mips
-#./build.sh
-#cp syncthing syncthing-mips
+export GOARCH=mips
+build
+mv syncthing syncthing-mips
